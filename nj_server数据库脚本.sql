6585403926ee587be5dee2f31d41ea93a47bacff
endureTang <358971749@qq.com>
2021-01-19 13:54:16 +0800

    库存汇总-活动功能模块-数据库脚本

--- nj_server数据库脚本.sql
+++ nj_server数据库脚本.sql
@@ -1,1 +1,10 @@
-ALTER TABLE `stock_format_dict` ADD COLUMN `sort` int NULL DEFAULT NULL AFTER `stock_format_id`;
+SET FOREIGN_KEY_CHECKS=0;
+
+CREATE TABLE `nj_server_bak`.`activity_stock`  (
+  `id` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `stock_no` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '参与活动的货号',
+  `activity_name` varchar(55) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT '' COMMENT '活动名称',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '活动商品表' ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS=1;
